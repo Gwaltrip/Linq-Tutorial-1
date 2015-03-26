2143b4b2aaff182f0b7d8da07b878d256ee627ba
Gwaltrip <gabrielwaltrip@gmail.com>
2015-03-26 01:34:15 -0700

    Main

---
+++ Linq.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq_Tutorial_1
+{
+    class Linq
+    {
+        private List<int> arr;
+
+        public Linq()
+        {
+            arr = (new int[] { 0, 5, 12, 15, 5, 11, 7, 23, 5, 1, 5, 3, 7 }).ToList();
+        }
+
+        public int arrayMax()
+        {
+            return arr.Max();
+        }
+
+        public int[] whereLessThan(int i)
+        {
+            return (from a in arr where a < i select  a).ToArray();
+        }
+
+        public int[] whereGreaterThan(int i)
+        {
+            return (from a in arr where a > i select a).ToArray();
+        }
+
+        public bool exists(int i)
+        {
+            return arr.Any(a => i == a);
+        }
+
+        public int amount(int i)
+        {
+            return (from a in arr where a == i select a).Count();
+        }
+
+        public bool addIfNotExists(int i)
+        {
+            if (!arr.Any(a => i == a))
+            {
+                arr.Add(i);
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Tutorial_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Linq l = new Linq();
+
+            Console.WriteLine("The max is " + l.arrayMax());
+            Console.WriteLine("Does 6 exists in the arr? " + l.exists(6));
+            Console.WriteLine("Does 5 exists in the arr? " + l.exists(5));
+            Console.WriteLine("How many times does 5 exists in the arr? " + l.amount(5));
+            Console.Write("What numbers are less than 5? " );
+            foreach (int i in l.whereLessThan(5))
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.Write("What nubmers are greater than 5? ");
+            foreach (int i in l.whereGreaterThan(5))
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Attempting to add 6, was it added? " + l.addIfNotExists(6));
+
+            Console.WriteLine("Attempting to add 5, was it added? " + l.addIfNotExists(5));
+
+
+            Console.ReadKey();
+        }
+    }
+}
